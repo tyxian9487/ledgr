f32986b5996ae19c680d7f64baa96b87558999b3
sausagemusic <sausagemusic15@gmail.com>
2026-05-23 14:19:19 +0000

    status bar fixed

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 export default function App() {
+  const colorScheme = useColorScheme();
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style={colorScheme === 'light' ? 'dark' : 'light'} />
     </View>
   );
 }
